9a6154e61a9b5a46c1862dec4f90990f25cf8e01
Avijit07x <deyavijit134@gmail.com>
2024-12-30 09:54:52 +0530

    add

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -1,13 +1,9 @@
-import { motion } from "framer-motion";
 import { ChevronDown } from "lucide-react";
 import { memo } from "react";
-import { Link } from "react-router";
-import EmailIcon from "../icons/EmailIcon";
-import GithubIcon from "../icons/GithubIcon";
-import LinkedinIcon from "../icons/LinkedinIcon";
-import XIcon from "../icons/XIcon";
+
 import BlurIn from "../ui/blur-in";
 import HyperText from "../ui/hyper-text";
+import Social from "./Social";
 
 const Hero = () => {
 	console.log("hero");
@@ -28,61 +24,7 @@ const Hero = () => {
 			>
 				Front-End Developer
 			</HyperText>
-			<motion.div
-				className="flex items-center justify-center gap-3 lg:gap-5"
-				initial="hidden"
-				animate="visible"
-				variants={{
-					hidden: { opacity: 0, y: 20 },
-					visible: {
-						opacity: 1,
-						y: 0,
-						transition: {
-							staggerChildren: 0.2,
-						},
-					},
-				}}
-			>
-				{[
-					{
-						href: "https://github.com/Avijit07x",
-						icon: <GithubIcon className="text-white" />,
-					},
-					{
-						href: "https://linkedin.com/in/avijit07x",
-						icon: <LinkedinIcon className="text-white" />,
-					},
-					{
-						href: "https://twitter.com/Avijit07x",
-						icon: <XIcon className="text-white" />,
-					},
-					{
-						href: "mailto:avijit@example.com",
-						icon: <EmailIcon className="text-white" />,
-					},
-				].map((item, index) => (
-					<motion.div
-						key={index}
-						variants={{
-							hidden: { opacity: 0, y: 20 },
-							visible: { opacity: 1, y: 0 },
-						}}
-						transition={{ duration: 0.5, ease: "easeInOut" }}
-					>
-						<Link to={item.href} target="_blank">
-							<div
-								className="flex size-12 items-center justify-center rounded-xl border border-[#FFFFFF20] shadow-2xl"
-								style={{
-									backdropFilter: "blur(16px) saturate(180%)",
-									backgroundColor: "rgba(17, 25, 40, 0.75)",
-								}}
-							>
-								{item.icon}
-							</div>
-						</Link>
-					</motion.div>
-				))}
-			</motion.div>
+			<Social />
 			<div className="absolute bottom-0 flex flex-col items-center gap-2 3xl:hidden">
 				<p>Scroll Down</p>
 				<div className="animate-bounce">
