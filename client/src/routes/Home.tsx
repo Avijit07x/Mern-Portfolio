9f1f38b59f903f352180235779eecda1305b143a
Avijit07x <deyavijit134@gmail.com>
2025-04-02 11:07:14 +0530

    test

--- client/src/routes/Home.tsx
+++ client/src/routes/Home.tsx
@@ -1,10 +1,13 @@
 import Hero from "@/components/hero/Hero";
 import Navbar from "@/components/navbar/Navbar";
 import Tools from "@/components/tools/Tools";
+import { useWindowWidth } from "@react-hook/window-size";
 import { ReactLenis } from "lenis/react";
 import { useCallback, useRef } from "react";
 const Home = () => {
 	const aboutRef = useRef<HTMLElement>(null);
+
+	const windowWidth = useWindowWidth();
 
 	const scrollToSection = useCallback(
 		(ref: React.RefObject<HTMLElement>) => {
@@ -14,15 +17,27 @@ const Home = () => {
 	);
 
 	return (
-		<ReactLenis root>
-			<div className="mx-auto h-full max-w-screen-2xl text-white">
-				<div className="3xl:h-[700px] relative z-50 h-svh max-h-svh min-h-svh w-full overflow-hidden bg-[#01031a00]">
-					<Navbar scrollToSection={scrollToSection} aboutRef={aboutRef} />
-					<Hero />
+		<>
+			{windowWidth > 1024 ? (
+				<ReactLenis root>
+					<div className="mx-auto h-full max-w-screen-2xl text-white">
+						<div className="3xl:h-[700px] relative z-50 h-svh max-h-svh min-h-svh w-full overflow-hidden bg-[#01031a00]">
+							<Navbar scrollToSection={scrollToSection} aboutRef={aboutRef} />
+							<Hero />
+						</div>
+						<Tools />
+					</div>
+				</ReactLenis>
+			) : (
+				<div className="mx-auto h-full max-w-screen-2xl text-white">
+					<div className="3xl:h-[700px] relative z-50 h-svh max-h-svh min-h-svh w-full overflow-hidden bg-[#01031a00]">
+						<Navbar scrollToSection={scrollToSection} aboutRef={aboutRef} />
+						<Hero />
+					</div>
+					<Tools />
 				</div>
-				<Tools />
-			</div>
-		</ReactLenis>
+			)}
+		</>
 	);
 };
 
