e63529c6437197f44950e7547e4b11e0deb840e1
Avijit07x <deyavijit134@gmail.com>
2025-04-04 00:08:58 +0530

    update : tools

--- client/src/components/tools/Tools.tsx
+++ client/src/components/tools/Tools.tsx
@@ -1,3 +1,4 @@
+import { useWindowWidth } from "@react-hook/window-size";
 import axios from "axios";
 import { Loader } from "lucide-react";
 import { motion } from "motion/react";
@@ -12,8 +13,8 @@ type Tool = {
 };
 
 const containerVariants = {
-	hidden: { opacity: 0, transition: { staggerChildren: 0.08 } },
-	visible: { opacity: 1, transition: { staggerChildren: 0.08 } },
+	hidden: { opacity: 0, transition: { staggerChildren: 0.07 } },
+	visible: { opacity: 1, transition: { staggerChildren: 0.07 } },
 };
 
 const itemVariants = {
@@ -24,6 +25,9 @@ const itemVariants = {
 const Tools = () => {
 	const [tools, setTools] = useState<Tool[]>([]);
 	const [loading, setLoading] = useState(true);
+
+	const windowWidth = useWindowWidth();
+
 	useEffect(() => {
 		const fetchTools = async () => {
 			try {
@@ -60,34 +64,53 @@ const Tools = () => {
 			</div>
 
 			{loading ? (
-				<div className="text-white flex items-center justify-center gap-2 min-h-[400px]">
-					<Loader className="animate-spin size-4" /> Loading...
+				<div className="flex min-h-[400px] items-center justify-center gap-2 text-white">
+					<Loader className="size-4 animate-spin" /> Loading...
 				</div>
 			) : (
-				<motion.div
-					className="mx-auto mt-5 flex max-w-screen-2xl flex-wrap items-center justify-center gap-4 lg:mt-10 xl:px-36"
-					variants={containerVariants}
-					initial="hidden"
-					whileInView="visible"
-					viewport={{ once: true, amount: 0.2 }}
-				>
-					{tools.map((tool) => (
+				<>
+					{windowWidth > 768 ? (
 						<motion.div
-							key={tool._id}
-							className="grid size-20 cursor-pointer place-items-center rounded-md border border-white/[0.1] bg-[#0f132e] text-lg drop-shadow-md will-change-transform"
-							title={tool.name}
-							variants={itemVariants}
-							whileHover={{ scale: 1.05, transition: { duration: 0.2 } }}
-							whileTap={{ scale: 0.95, transition: { duration: 0.2 } }}
+							className="mx-auto mt-5 flex max-w-screen-2xl flex-wrap items-center justify-center gap-4 lg:mt-10 xl:px-36"
+							variants={containerVariants}
+							initial="hidden"
+							whileInView="visible"
+							viewport={{ once: true, amount: 0.2 }}
 						>
-							<img
-								className="aspect-auto size-11.5 object-contain"
-								src={tool.image.url}
-								alt={tool.name}
-							/>
+							{tools.map((tool) => (
+								<motion.div
+									key={tool._id}
+									className="grid size-20 cursor-pointer place-items-center rounded-md border border-white/[0.1] bg-[#0f132e] text-lg drop-shadow-md will-change-transform"
+									title={tool.name}
+									variants={itemVariants}
+									whileHover={{ scale: 0.8, transition: { duration: 0.2 } }}
+								>
+									<img
+										className="aspect-auto size-11.5 object-contain"
+										src={tool.image.url}
+										alt={tool.name}
+									/>
+								</motion.div>
+							))}
 						</motion.div>
-					))}
-				</motion.div>
+					) : (
+						<div className="mx-auto mt-5 flex max-w-screen-2xl flex-wrap items-center justify-center gap-4 lg:mt-10 xl:px-36">
+							{tools.map((tool) => (
+								<div
+									key={tool._id}
+									className="grid size-20 cursor-pointer place-items-center rounded-md border border-white/[0.1] bg-[#0f132e] text-lg drop-shadow-md will-change-transform"
+									title={tool.name}
+								>
+									<img
+										className="aspect-auto size-11.5 object-contain"
+										src={tool.image.url}
+										alt={tool.name}
+									/>
+								</div>
+							))}
+						</div>
+					)}
+				</>
 			)}
 		</div>
 	);
--- client/src/routes/Home.tsx
+++ client/src/routes/Home.tsx
@@ -1,13 +1,10 @@
 import Hero from "@/components/hero/Hero";
 import Navbar from "@/components/navbar/Navbar";
 import Tools from "@/components/tools/Tools";
-import { useWindowWidth } from "@react-hook/window-size";
 import { ReactLenis } from "lenis/react";
 import { useCallback, useRef } from "react";
 const Home = () => {
 	const aboutRef = useRef<HTMLElement>(null);
-
-	const windowWidth = useWindowWidth();
 
 	const scrollToSection = useCallback(
 		(ref: React.RefObject<HTMLElement>) => {
@@ -18,17 +15,7 @@ const Home = () => {
 
 	return (
 		<>
-			{windowWidth > 1024 ? (
-				<ReactLenis root>
-					<div className="mx-auto h-full max-w-screen-2xl text-white">
-						<div className="3xl:h-[700px] relative z-50 h-svh max-h-svh min-h-svh w-full overflow-hidden bg-[#01031a00]">
-							<Navbar scrollToSection={scrollToSection} aboutRef={aboutRef} />
-							<Hero />
-						</div>
-						<Tools />
-					</div>
-				</ReactLenis>
-			) : (
+			<ReactLenis root>
 				<div className="mx-auto h-full max-w-screen-2xl text-white">
 					<div className="3xl:h-[700px] relative z-50 h-svh max-h-svh min-h-svh w-full overflow-hidden bg-[#01031a00]">
 						<Navbar scrollToSection={scrollToSection} aboutRef={aboutRef} />
@@ -36,7 +23,7 @@ const Home = () => {
 					</div>
 					<Tools />
 				</div>
-			)}
+			</ReactLenis>
 		</>
 	);
 };
