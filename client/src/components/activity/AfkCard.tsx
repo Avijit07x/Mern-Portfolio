89b74c772d0942e13d9e57ab0abca1e65f040a3d
Avijit07x <deyavijit134@gmail.com>
2025-04-19 10:36:54 +0530

    fix : afk card

--- client/src/components/activity/AfkCard.tsx
+++ client/src/components/activity/AfkCard.tsx
@@ -1,6 +1,6 @@
 const AfkCard = () => {
 	return (
-		<div className="flex w-fit justify-center gap-2.5 rounded-lg bg-gray-900 p-4 text-sm font-medium text-white shadow-lg">
+		<div className="flex w-fit justify-center gap-2.5 text-start rounded-lg bg-gray-900 p-4 text-sm font-medium text-white shadow-lg">
 			<div className="relative size-18">
 				<img className="h-full w-full rounded-md" src="/afk.webp" alt="afk" />
 			</div>
