a400e3a32cf490109b03ba6331861640dd28d841
Avijit07x <deyavijit134@gmail.com>
2025-05-20 09:14:41 +0530

    fix : tool

--- client/src/components/tools/Tools.tsx
+++ client/src/components/tools/Tools.tsx
@@ -1,4 +1,3 @@
-import { useWindowWidth } from "@react-hook/window-size";
 import axios from "axios";
 
 import { useEffect, useState } from "react";
@@ -15,7 +14,6 @@ export type Tool = {
 const Tools = () => {
 	const [tools, setTools] = useState<Tool[]>([]);
 	const [loading, setLoading] = useState(true);
-	const windowWidth = useWindowWidth();
 
 	const fetchTools = async () => {
 		try {
@@ -53,31 +51,7 @@ const Tools = () => {
 				</div>
 			</div>
 
-			{loading ? (
-				<ToolsLoader />
-			) : (
-				<>
-					{windowWidth >= 1280 ? (
-						<AnimatedTools tools={tools} />
-					) : (
-						<div className="mx-auto mt-5 flex max-w-screen-2xl flex-wrap items-center justify-center gap-4 px-3 sm:px-10 lg:mt-10 xl:px-36">
-							{tools.map((tool) => (
-								<div
-									key={tool._id}
-									className="grid size-20 place-items-center rounded-md border border-white/[0.1] bg-[#0f132e] text-lg drop-shadow-md"
-									title={tool.name}
-								>
-									<img
-										className="aspect-auto size-11.5 object-contain"
-										src={tool.image.url}
-										alt={tool.name}
-									/>
-								</div>
-							))}
-						</div>
-					)}
-				</>
-			)}
+			{loading ? <ToolsLoader /> : <AnimatedTools tools={tools} />}
 		</div>
 	);
 };
