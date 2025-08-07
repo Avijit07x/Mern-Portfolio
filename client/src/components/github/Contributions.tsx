e16e141c97a27e523a710d38e8c560cf084d71c5
Avijit07x <deyavijit134@gmail.com>
2025-08-07 12:56:43 +0530

    update : github graph

--- client/src/components/github/Contributions.tsx
+++ client/src/components/github/Contributions.tsx
@@ -1,15 +1,7 @@
-import { useEffect, useState } from "react";
-import GithubGraph from "./GithubGraph";
+import GitHubCalendar from "react-github-calendar";
 
 const Contributions: React.FC = () => {
-	const [contributions, setContributions] = useState([]);
 
-	useEffect(() => {
-		fetch("https://github-contributions-api.jogruber.de/v4/avijit07x?y=last")
-			.then((res) => res.json())
-			.then((data) => setContributions(data.contributions))
-			.catch(console.error);
-	}, []);
 
 	return (
 		<div>
@@ -17,8 +9,15 @@ const Contributions: React.FC = () => {
 				GitHub Contributions
 			</h2>
 
-			<div className="w-full max-lg:overflow-x-scroll">
-				<GithubGraph contributions={contributions} />
+			<div className="mt-5 flex w-full items-center justify-center px-4">
+				<GitHubCalendar
+					username="avijit07x"
+					blockSize={10}
+					theme={{
+						light: ["#050817", "#2e3355", "#6d73aa", "#a2a8d8", "#a2a8d8"],
+						dark: ["#050817", "#2e3355", "#6d73aa", "#a2a8d8", "#a2a8d8"],
+					}}
+				/>
 			</div>
 		</div>
 	);
