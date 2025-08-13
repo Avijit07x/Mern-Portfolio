3f25b7b9c53b9e105072c53ff25fe3fadcb01325
Avijit07x <deyavijit134@gmail.com>
2025-08-13 09:15:25 +0530

    update: add sitemap plugin and optimize build configuration

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -19,4 +19,36 @@ export default defineConfig({
 			"@": path.resolve(__dirname, "./src"),
 		},
 	},
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks: {
+					react: ["react", "react-dom", "react-router"],
+					"radix-ui": [
+						"@radix-ui/react-avatar",
+						"@radix-ui/react-dialog",
+						"@radix-ui/react-dropdown-menu",
+						"@radix-ui/react-label",
+						"@radix-ui/react-slot",
+						"lucide-react",
+					],
+					redux: ["@reduxjs/toolkit", "react-redux"],
+					"dnd-kit": [
+						"@dnd-kit/core",
+						"@dnd-kit/modifiers",
+						"@dnd-kit/sortable",
+						"@dnd-kit/utilities",
+					],
+					utils: [
+						"axios",
+						"clsx",
+						"class-variance-authority",
+						"tailwind-merge",
+						"zod",
+						"emblor",
+					],
+				},
+			},
+		},
+	},
 });
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import ReactCompiler from "babel-plugin-react-compiler";
 import path from "path";
 import { defineConfig } from "vite";
+import Sitemap from "vite-plugin-sitemap";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,10 +14,40 @@ export default defineConfig({
 			},
 		}),
 		tailwindcss(),
+		Sitemap({ hostname: "https://avijit07x.vercel.app" }),
 	],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+	build: {
+		chunkSizeWarningLimit: 1000, // warning threshold ko 1MB kar diya
+
+		rollupOptions: {
+			output: {
+				manualChunks: {
+					react: ["react", "react-dom", "react-router"],
+					"radix-ui": [
+						"@radix-ui/react-dialog",
+						"@radix-ui/react-label",
+						"@radix-ui/react-separator",
+						"@radix-ui/react-slot",
+						"lucide-react",
+					],
+					github: ["react-github-calendar"],
+					animations: ["lenis", "motion"],
+					utils: [
+						"axios",
+						"clsx",
+						"class-variance-authority",
+						"tailwind-merge",
+						"zod",
+						"@hookform/resolvers",
+					],
+					vercel: ["@vercel/analytics", "@vercel/speed-insights"],
+				},
+			},
 		},
 	},
 });
