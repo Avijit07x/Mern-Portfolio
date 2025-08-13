a2d80353083f956c7e145b18e62d8698c46e92a5
Avijit07x <deyavijit134@gmail.com>
2025-08-13 09:25:32 +0530

    refactor: update dependencies and remove unused packages
    - Removed "@tailwindcss/vite" and "tailwindcss" from dependencies in admin and client package.json files.
    - Added "@tailwindcss/vite" and "tailwindcss" to devDependencies in admin and client package.json files.
    - Removed "sonner" import and its usage from client/src/main.tsx.
    - Updated client/package-lock.json to reflect the changes in dependencies.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,6 @@ import { SpeedInsights } from "@vercel/speed-insights/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
-import { Toaster } from "sonner";
 import App from "./App.tsx";
 import RefContextProvider from "./context/RefContextProvider.tsx";
 import "./index.css";
@@ -13,7 +12,6 @@ createRoot(document.getElementById("root")!).render(
 		<RefContextProvider>
 			<BrowserRouter>
 				<App />
-				<Toaster position="bottom-right" richColors />
 				<Analytics />
 				<SpeedInsights />
 			</BrowserRouter>
